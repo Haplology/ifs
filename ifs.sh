9ccc12717846951289d6ae8bd3900e4321344101
Haplology <chandler.jesse@gmail.com>
2017-09-29 16:39:19 +0900

    convert & do image post-processing single command

--- ifs.sh
+++ ifs.sh
@@ -20,23 +20,10 @@ do
     echo "generate fractal and convert to SVG"
     ./ifs | ./points2svg > ifs.svg
 
-    while [ 0 -ne $(ps -e | grep -c 'mogrify') ]
-    do
-	sleep 10
-	echo "waiting for a mogrify job to finish..."
-    done
-    
-    echo "convert image format"
-    mogrify -format png ifs.svg
-
-    echo "resize, normalize, and flip"
+    echo "convert to PNG, resize, normalize, and flip"
     FILENAME="ifs""$(date +%s)"".png"
-    convert -resize 1400x1400 -normalize -flip ifs.png "$FILENAME"
+    convert -format png -resize 1400x1400 -normalize -flip ifs.svg "$FILENAME"
     # display "$FILENAME" &
-
-    echo "housekeeping"
-    rm ifs.svg
-    rm ifs.png
 
     END=$(date +%s)
     DIFF=$(echo "$END - $START" | bc)
