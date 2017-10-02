644d006e2abccafe646cc0f9d8f9c0965f0e95a3
Haplology <chandler.jesse@gmail.com>
2017-10-02 21:01:05 +0900

    save points to pixels and print SVG

--- ifs.c
+++ ifs.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <float.h>
 
-#define ITERATIONS 12
-#define SIZE 2000
-#define MAX_TRANSFORMATIONS 4
-#define COLOR_DECAY 1.0
+#define ITERATIONS 11
+#define STARTPOINT 100000
+#define RESOLUTION 3000
+#define MAX_TRANSFORMATIONS 6
+#define PADDING 0.01
+#define MINSCALE 0.2
+#define MAXSCALE 0.7
+
 
 static double rotate[MAX_TRANSFORMATIONS]; 
 static double xscale[MAX_TRANSFORMATIONS];
@@ -16,12 +21,13 @@ static double yshift[MAX_TRANSFORMATIONS];
 static double palette_red[MAX_TRANSFORMATIONS]; 
 static double palette_green[MAX_TRANSFORMATIONS]; 
 static double palette_blue[MAX_TRANSFORMATIONS]; 
+static short int pixelrgb[RESOLUTION*RESOLUTION*3] = {0};
 
 
 
 double xstar(double x, double y, double rotate, double xscale, double xshift);
 double ystar(double x, double y, double rotate, double yscale, double yshift);
-void iterateFS(int iterations_left, double x, double y, double red, double green, double blue, int transformations);
+void iterateFS(int iterations_left, double x, double y, double red, double green, double blue, int transformations, double xmin, double ymin, double xmax, double ymax, short int pixelrgb[]);
 void find_bounds(int iterations_left, double x, double y, double *xminp, double *yminp, double *xmaxp, double *ymaxp, int transformations);
 double random_angle();
 double normalized_random();
@@ -29,6 +35,9 @@ double random_scale();
 double random_shift();
 int random_color();
 int random_number_of_transformations();
+
+
+
 
 double xstar(double x, double y, double rotate, double xscale, double xshift) {
   return ((x*cos(rotate) - y*sin(rotate))*xscale + xshift);
@@ -38,21 +47,43 @@ double ystar(double x, double y, double rotate, double yscale, double yshift) {
   return ((y*cos(rotate) + x*sin(rotate))*yscale + yshift);
 }
 
-void iterateFS(int iterations_left, double x, double y, double red, double green, double blue, int transformations) {
+void iterateFS(int iterations_left, double x, double y, double red, double green, double blue, int transformations, double xmin, double ymin, double xmax, double ymax, short int pixelrgb[]) {
   if (iterations_left == 0) {
-    /* stop recursion and print out final values of x and y */
-    printf("%f\t%f\t%f\t%f\t%f\n", x, y, red, green, blue);
+    /* stop recursion and save in pixel space */
+    double width = xmax - xmin;
+    double height = ymax - ymin;
+    double greater;
+    double lesser;
+    double xshift = 0.0;
+    double yshift = 0.0;
+    if (width > height) {
+      greater = width;
+      lesser = height;
+      yshift = (greater - lesser)*0.5;
+    } else {
+      greater = height;
+      lesser = width;
+      xshift = (greater - lesser)*0.5;
+    }
+
+    int xpixel = (int)((((x-xmin+xshift)/greater)*RESOLUTION)*(1.0-PADDING*2.0)+RESOLUTION*PADDING);
+    int ypixel = (int)((((y-ymin+yshift)/greater)*RESOLUTION)*(1.0-PADDING*2.0)+RESOLUTION*PADDING);
+    pixelrgb[RESOLUTION*3*xpixel + 3*ypixel + 0] = (int)red;
+    pixelrgb[RESOLUTION*3*xpixel + 3*ypixel + 1] = (int)green;
+    pixelrgb[RESOLUTION*3*xpixel + 3*ypixel + 2] = (int)blue;
   } else {
     /* iterate FS recursively */
     int i;
     for( i = 0; i < transformations; i = i + 1 ) {
       iterateFS(iterations_left-1,
-		xstar(x, y, rotate[i], xscale[i], xshift[i]*SIZE),
-		ystar(x, y, rotate[i], yscale[i], yshift[i]*SIZE),
-		(red*COLOR_DECAY + palette_red[i])*0.5,
-		(green*COLOR_DECAY + palette_green[i])*0.5,
-		(blue*COLOR_DECAY + palette_blue[i])*0.5,
-		transformations
+		xstar(x, y, rotate[i], xscale[i], xshift[i]*STARTPOINT),
+		ystar(x, y, rotate[i], yscale[i], yshift[i]*STARTPOINT),
+		(red + palette_red[i])*0.5,
+		(green + palette_green[i])*0.5,
+		(blue + palette_blue[i])*0.5,
+		transformations, 
+		xmin, ymin, xmax, ymax,
+		pixelrgb
 		);
     }
   }
@@ -79,8 +110,8 @@ void find_bounds(int iterations_left, double x, double y, double *xminp, double 
     int i;
     for( i = 0; i < transformations; i = i + 1 ) {
       find_bounds(iterations_left-1,
-		  xstar(x, y, rotate[i], xscale[i], xshift[i]*SIZE),
-		  ystar(x, y, rotate[i], yscale[i], yshift[i]*SIZE),
+		  xstar(x, y, rotate[i], xscale[i], xshift[i]*STARTPOINT),
+		  ystar(x, y, rotate[i], yscale[i], yshift[i]*STARTPOINT),
 		  xminp, yminp, xmaxp, ymaxp,
 		  transformations);
     }
@@ -101,11 +132,7 @@ double random_angle() {
 }
 
 double random_scale() {
-  double r = normalized_random();
-  double min_scale = 0.4;
-  double max_scale = 0.6;
-  double scale_range = max_scale - min_scale;
-  return (min_scale + scale_range*r);
+  return (MINSCALE + (MAXSCALE - MINSCALE)*normalized_random()); 
 }
 
 double random_shift() {
@@ -122,6 +149,15 @@ int random_number_of_transformations() {
   return (min_transformations + (rand() % (MAX_TRANSFORMATIONS-min_transformations+1)));
 }
 
+
+
+
+
+
+
+
+
+
 int main() {
   srand(time(NULL)); 
 
@@ -137,15 +173,39 @@ int main() {
     palette_blue[i] = random_color();
   }
 
-  int transformations = random_number_of_transformations();
+  int transformations = random_number_of_transformations();  
   
-  double xmin = 100000.0;
-  double ymin = 100000.0;
-  double xmax = -100000.0;
-  double ymax = -100000.0;
-  find_bounds(ITERATIONS, SIZE, SIZE, &xmin, &ymin, &xmax, &ymax, transformations);
-  printf("%f\t%f\t%f\t%f\n", xmin, ymin, xmax, ymax);
+  double xmin = FLT_MAX;
+  double ymin = FLT_MAX;
+  double xmax = -1.0*FLT_MAX;
+  double ymax = -1.0*FLT_MAX;
+  find_bounds(ITERATIONS, STARTPOINT, STARTPOINT, &xmin, &ymin, &xmax, &ymax, transformations);
 
-  iterateFS(ITERATIONS, SIZE, SIZE, 255.0, 255.0, 255.0, transformations);
+  iterateFS(ITERATIONS, STARTPOINT, STARTPOINT, 255.0, 255.0, 255.0, transformations, xmin, ymin, xmax, ymax, pixelrgb);
+
+  printf("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+  printf("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"100%%\" height=\"100%%\" viewBox=\"0 0 %d %d\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n", RESOLUTION, RESOLUTION);
+  printf("    <rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" fill=\"black\" />\n", RESOLUTION, RESOLUTION);
+  
+  int x;
+  int y;
+  short int red;
+  short int green;
+  short int blue;
+  for ( x = 0; x < RESOLUTION; x++ ) {
+    for ( y = 0; y < RESOLUTION; y++ ) {
+      red = pixelrgb[RESOLUTION*3*x + 3*y + 0];
+      green = pixelrgb[RESOLUTION*3*x + 3*y + 1];
+      blue = pixelrgb[RESOLUTION*3*x + 3*y + 2];
+      if ((red != 0) || (green != 0) || (red != 0)) {
+	printf("    <circle cx=\"%d\" cy=\"%d\" r=\"0.4\" fill=\"rgb(%d, %d, %d)\" />\n",
+	       x, y,
+	       red, green, blue);
+      }
+    }
+  }
+  printf("\n</svg>\n");
+
+
   return(0);
 }
