199d9ff2b7395e09f23286db5ffd2b3d6b09fe77
Haplology <chandler.jesse@gmail.com>
2023-09-30 11:27:50 +0900

    do one frame at a time

--- animation.py
+++ animation.py
@@ -1,19 +1,9 @@
 import random, codecs, math
 from scipy import interpolate
-#import matplotlib.pyplot as plt
-
-TEST = False
-
 NO_FUNCTIONS = 4 # the number of transformations, like 3 or 4
-SPLITS = 6 # number of processes to fork into
-if TEST == True:
-    BETWEEN = 50
-    SEQUENCE_LENGTH = 4 # the number of key frames to interpolate between
-    ITERATIONS = 9
-else:
-    BETWEEN = 800 # the number of frames between key frames
-    SEQUENCE_LENGTH = 10
-    ITERATIONS = 13
+BETWEEN = 800 # the number of frames between key frames
+SEQUENCE_LENGTH = 4
+ITERATIONS = 13
 
 # make NO_FUNCTIONS copies of a spline for each parameter
 # the number of points on the spline is SEQUENCE_LENGTH
@@ -38,9 +28,7 @@ for param in ["rotate", "xscale", "yscale", "xshift", "yshift", "red", "green", 
         x = range(0, SEQUENCE_LENGTH*BETWEEN, BETWEEN)
         tck = interpolate.splrep(x, spline_points)
         splines[param+str(function_number)] = tck
-
-#print(splines)
-
+                                                                                                                         
 ifs_calls = []
 x = []
 y = []
@@ -54,26 +42,14 @@ for frame in range(0,(SEQUENCE_LENGTH-1)*BETWEEN):
                 x.append(frame)
                 y.append(interpolate.splev(frame, splines["xscale1"]))
     command += "--iterations "+str(ITERATIONS)
-    #print(command)
     ifs_calls.append(command)
 
-#plt.plot(x, y)
-#plt.show()
-
-#render_sequence = range((SEQUENCE_LENGTH-1)*BETWEEN)
-#random.shuffle(render_sequence)
-
 commands = []
-#commands.append("#!/bin/bash")
-#wait_for_memory = "while [ $(free -m | sed '2q;d' | awk -v N=7 '{print $N}' ) -lt 10000 ]; do sleep 3; done; "
-wait_for_memory = "while [ $(ps | grep -c 'convert' ) -ne 0 ]; do sleep $(($RANDOM%10 + 3)); done; "
+commands.append("#!/bin/zsh")
 for frame_number in range((SEQUENCE_LENGTH-1)*BETWEEN):
     head = "frame" + str(frame_number).zfill(4)
     png = head + ".png"
-    commands.append('if [ ! -s ./'+png+' ]; then '+ifs_calls[frame_number]+' --filename '+str(frame_number%SPLITS)+'; '+ wait_for_memory + 'convert -verbose -format png -resize 3840x2160 -normalize -flip ifs'+str(frame_number%SPLITS)+'.bmp '+png+'; fi')
+    commands.append('if [ ! -s ./'+png+' ]; then date; '+ifs_calls[frame_number]+ '; convert -verbose -format png -resize 3840x2160 -normalize -flip ifs.bmp '+png+'; fi')
 
-
-commands.append("while [ $(ls *.png | wc -l) -lt " + str((SEQUENCE_LENGTH-1)*BETWEEN) + " ]; do sleep 10; done; sleep 30; ffmpeg -r 30 -i frame%04d.png movie.mp4; totem movie.mp4")
+commands.append("ffmpeg -r 30 -i frame%04d.png movie.mp4")
 codecs.open("render.sh", "w", "utf8").write("\n".join(commands))
-
-print('for F in $(seq 0 '+str(SPLITS-1)+'); do cat render.sh | awk ' + "'NR%6=='" + '"$F" > fork"$F".sh; (bash fork"$F".sh &); sleep 3; done')
